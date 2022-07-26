b55c209054b869349cfa6d8b24cc95349dc15dfe
HM <95879255+HMser@users.noreply.github.com>
2022-07-26 12:45:12 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,6 @@
 let handler =  m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
-╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+╭─「 Donate To developer 」
+│ • https://www.buymeacoffee.com/hmser
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
